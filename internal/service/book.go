f9fc148b25c6598a1d122ecd8b1534239a38cb47
João Victor da Silva <jv782063@gmail.com>
2024-02-17 13:42:42 -0300

    create book streams

--- internal/service/book.go
+++ internal/service/book.go
@@ -2,6 +2,7 @@ package service
 
 import (
 	"context"
+	"io"
 
 	"github.com/joaovds/learn-grpc-go/internal/db"
 	"github.com/joaovds/learn-grpc-go/internal/pb"
@@ -65,4 +66,57 @@ func (b *BookService) GetBook(ctx context.Context, in *pb.GetByIdBook) (*pb.Book
     Isbn:        book.ISBN,
     AuthorId:    book.AuthorID,
   }, nil
+}
+
+func (b *BookService) CreateBookStream(stream pb.BookService_CreateBookStreamServer) error {
+  books := make([]*pb.Book, 4)
+
+  for {
+    book, err := stream.Recv()
+    if err == io.EOF {
+      return stream.SendAndClose(&pb.BookList{Books: books})
+    }
+    if err != nil {
+      return err
+    }
+
+    result, err := b.BookDB.Create(book.Name, book.Isbn, book.AuthorId)
+    if err != nil {
+      return err
+    }
+
+    books = append(books, &pb.Book{
+      Id:          result.ID,
+      Name:        result.Name,
+      Isbn:        result.ISBN,
+      AuthorId:    result.AuthorID,
+    })
+  }
+}
+
+func (b *BookService) CreateBookStreamBidirectional(stream pb.BookService_CreateBookStreamBidirectionalServer) error {
+  for {
+    book, err := stream.Recv()
+    if err == io.EOF {
+      return nil
+    }
+    if err != nil {
+      return err
+    }
+
+    result, err := b.BookDB.Create(book.Name, book.Isbn, book.AuthorId)
+    if err != nil {
+      return err
+    }
+
+    err = stream.Send(&pb.Book{
+      Id:          result.ID,
+      Name:        result.Name,
+      Isbn:        result.ISBN,
+      AuthorId:    result.AuthorID,
+    })
+    if err != nil {
+      return err
+    }
+  }
 }
